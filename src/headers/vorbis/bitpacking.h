9ea75182b4a1a430e8eed168862c7b001de4ef22
Lt1Gt0 <simonkaminski1100@gmail.com>
2022-08-02 19:47:03 -0700

    Bitpacking stubs

--- src/headers/vorbis/bitpacking.h
+++ src/headers/vorbis/bitpacking.h
@@ -4,17 +4,45 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <vector>
+#include <exception>
 
 typedef uint8_t byte;
 typedef byte octet;
 
-struct ByteStream {
-    byte*   stream;
-    size_t  streamLength;
-    uint8_t bitCursor;
+/* the maximum number of bits that are contained when writing to a byte
+ * stream from the view of OGG is 32 bits */
 
-    void    Write(size_t data); 
-    size_t  Read(size_t byteOffset, uint8_t bitCursor);   
+// FIXME
+// Signed and unsigned data need to be handled based off of context in the
+// stream, which I do not know how to handle as of now meaning everything
+// in the bytestream is treated as unsigned data
+class ByteStream {
+    public:
+        std::vector<byte>   mStream;
+    private:
+        uint8_t             mBitCursor;
+        size_t              mByteOffset;
+
+    public:
+        ByteStream();
+        ~ByteStream();
+        
+        void        Write(uint32_t _data); 
+        byte        ReadByte();
+        byte        ReadByte(size_t _byteOffset, uint8_t _bitCursor);
+        uint16_t    ReadWord();
+        uint16_t    ReadWord(size_t _byteOffset, uint8_t _bitCursor);
+        uint32_t    Read(size_t _bitCount);   
+        uint32_t    Read(size_t _byteOffset, uint8_t _bitCursor, size_t _bitCount);   
+    private:
+        void SetBit(byte _byte, uint8_t _pos, uint8_t _val);
+
+        /* ---------- EXCEPTIONS ---------- */  
+        class OutOfRange : public std::exception
+        {
+            virtual const char* what() const throw(); 
+        } out_of_range;
 };
 
 #endif // _VORBIS_BITPACKING_H
--- src/vorbis/bitpacking.cpp
+++ src/vorbis/bitpacking.cpp
@@ -1,6 +1,77 @@
 #include "vorbis/bitpacking.h"
 
-int ReadBytestream()
+ByteStream::ByteStream()
 {
-    return 0;
+    this->mStream = std::vector<byte>();
+    this->mBitCursor = 0;
+    this->mByteOffset = 0;
 }
+
+ByteStream::~ByteStream()
+{
+
+}
+
+/* This method should mainly be used if I was working
+ * on an encoder so I will leave it here as is */
+void ByteStream::Write(uint32_t _data)
+{
+
+}
+
+byte ByteStream::ReadByte()
+{
+    return ReadByte(this->mByteOffset, this->mBitCursor); 
+}
+
+byte ByteStream::ReadByte(size_t _byteOffset, uint8_t _bitCursor)
+{
+    this->mByteOffset = _byteOffset;
+    this->mBitCursor = _bitCursor;
+
+    /* TODO */
+}
+
+uint16_t ByteStream::ReadWord()
+{
+    return ReadWord(this->mByteOffset, this->mBitCursor); 
+}
+
+uint16_t ByteStream::ReadWord(size_t _byteOffset, uint8_t _bitCursor)
+{
+    this->mByteOffset = _byteOffset;
+    this->mBitCursor = _bitCursor;
+
+    /* TODO */
+}
+
+uint32_t ByteStream::Read(size_t _bitCount)
+{
+    uint32_t data;
+    
+    for (size_t i = 0; i < _bitCount; i++) {
+        if (this->mBitCursor == 8) {
+            this->mByteOffset++;  
+        } 
+    }
+
+    return data;
+}
+
+uint32_t ByteStream::Read(size_t _byteOffset, uint8_t _bitCursor, size_t _bitCount)
+{
+    this->mBitCursor = _bitCursor;
+    this->mByteOffset = _byteOffset;
+    return Read(_bitCount);
+}
+
+void SetBit(byte _byte, uint8_t _pos, uint8_t _val)
+{
+    
+}
+
+/* ---------- EXCEPTIONS ---------- */  
+const char* ByteStream::OutOfRange::what() const throw()
+{
+    return "Out of range";
+} 
